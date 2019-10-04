a869410e60dbcdf0964a467585a94ba666f56798
Kat Maldon <katharinamaldon@gmail.com>
2019-10-04 17:18:15 +0000

    Automatically backed up by Learn

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,8 +1,1 @@
-class CreateDogs < ActiveRecord::Migration[4.2]
-  def change
-    create_table :dogs do |t|
-      t.string :name
-      t.string :breed
-    end
-  end
-end
+class HauntedHouse < ActiveRecord::Base 
